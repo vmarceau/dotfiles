a220c18cb754d27e1cf68afbbf6eaac58d8ceaa2
vmarceau <vincent.marceau@auth0.com>
2023-05-20 20:28:16 -0400

    update

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -2,6 +2,6 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({ extra_args = { "--print-width 120" } }),
     },
 })
